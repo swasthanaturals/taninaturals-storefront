fcc7678914ca2ab620947afe493f60a5145240c9
konidev20 <sgovind.dev@outlook.com>
2021-08-08 22:51:03 +0530

    Initialized the project repository

---
+++ src/layouts/Layout.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function Layout({ children }) {
+    return (
+        <>
+            <Header />
+            <main>
+                {children}
+            </main>
+            <Footer />
+        </>
+    );
+}
+
+export default Layout;
---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import "../styles/css/global.css"
+import Layout from "../layouts/Layout"
+
+const IndexPage = () => (
+  <Layout>
+    <h1 className="text-3xl">Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+  </Layout>
+)
+
+export default IndexPage
